5eec0031e2502b3ed182c0469d555671afd5e9c4
Petter Grø Rein <pettegre@stud.ntnu.no>
2018-02-12 13:59:59 +0100

    Fikset dpendixy

--- app.ui/src/main/java/tdt4140/gr1844/app/ui/HelloWorld.java
+++ app.ui/src/main/java/tdt4140/gr1844/app/ui/HelloWorld.java
@@ -1,4 +1,14 @@
 package tdt4140.gr1844.app.ui;
 
 public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+    public boolean HelloWorld() {
+        return false;
+    }
+
 }
--- app.ui/src/test/java/tdt4140/gr1844/app/ui/HelloWorldTest.java
+++ app.ui/src/test/java/tdt4140/gr1844/app/ui/HelloWorldTest.java
@@ -1,4 +1,27 @@
 package tdt4140.gr1844.app.ui;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import tdt4140.gr1844.app.core.HelloWorld;
+
+
 public class HelloWorldTest {
+
+    private tdt4140.gr1844.app.core.HelloWorld app;
+
+    @Before
+    public void setUp() {
+        app = new HelloWorld();
+    }
+
+
+    @Test
+    public void testHello() {
+        Assert.assertFalse(app.HelloWorld());
+    }
 }
