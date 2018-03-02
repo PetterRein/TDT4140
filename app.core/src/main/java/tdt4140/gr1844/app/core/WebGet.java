eb242ecab41e95e62c1a9f2fb528c1b263cc5a48
Petter Grø Rein <pettegre@stud.ntnu.no>
2018-03-02 10:50:16 +0100

    Skrev enkel kode for å ta ut user og password fra get call

--- app.core/src/main/java/tdt4140/gr1844/app/core/WebGet.java
+++ app.core/src/main/java/tdt4140/gr1844/app/core/WebGet.java
@@ -10,35 +10,47 @@ import javax.ws.rs.GET;
 
 public class WebGet extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("Hei");
 
     }
 
     @GET
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws IOException, ServletException {
-        // Set the response message's MIME type.
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         response.setContentType("text/html;charset=UTF-8");
-        // Allocate a output writer to write the response message into the network socket.
-        PrintWriter out = response.getWriter();
+        String path = request.getPathInfo();
+        if (path != null) {
+            if (path.startsWith("/")) {
+                path = path.substring(1);
+                String[] segments = path.split("\\/");
+                if(segments.length == 2){
+                    String userlogUs = segments[0].toLowerCase();
+                    String userlogPa = segments[1].toLowerCase();
+                    if (userlogUs.startsWith("user") && userlogPa.startsWith("password")){
+                        String user = segments[0].substring(5);
+                        String passsword = segments[1].substring(9);
+                        System.out.println(user + " " + passsword);
+                    }
+                }
+            }
+           /** String[] segments = path.split("\\/");
+            if (segments.length == 1) {
+                System.out.println(segments[0]);
+            }**/
+        }
 
-        // Use a ResourceBundle for localized string in "LocalStrings_xx.properties" for i18n.
-        // The request.getLocale() sets the locale based on the "Accept-Language" request header.
-        // To test other locales.
-        //ResourceBundle rb = ResourceBundle.getBundle("LocalStrings", new Locale("fr"));
-
-        // Write the response message, in an HTML document.
+            PrintWriter out = response.getWriter();
         try {
             out.println("<!DOCTYPE html>");  // HTML 5
             out.println("<html><head>");
             out.println("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>");
             out.println("<title>"  + "</title></head>");
             out.println("<body>");
-            out.println("<h1>"  + "</h1>");  // Prints "Hello, world!"
-            // Set a hyperlink image to refresh this page
+            String h1 = "Hello World";
+            out.println("<h1>" + request + "  TESTWST  " + response + "</h1>");  // Prints "Hello, world!"
             out.println("<a href='" + request.getRequestURI() + "'><img src='images/return.gif'></a>");
             out.println("</body></html>");
         } finally {
-            out.close();  // Always close the output writer
+            out.close();
         }
     }
 }
