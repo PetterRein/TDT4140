8a9a7fdafacc8f10a2dc76ea413f4f9e9ce84544
Petter Grø Rein <pettegre@stud.ntnu.no>
2018-03-02 12:49:39 +0100

    La til Post #17

--- app.core/src/main/java/tdt4140/gr1844/app/core/WebGet.java
+++ app.core/src/main/java/tdt4140/gr1844/app/core/WebGet.java
@@ -1,16 +1,34 @@
 package tdt4140.gr1844.app.core;
 
+import sun.net.www.http.HttpClient;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import javax.ws.rs.GET;
 
 public class WebGet extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("Hei");
+        System.out.println(request.getParameterNames());
+        System.out.println(request.getHeaderNames());
+        System.out.println(request.getQueryString());
+       /** System.out.println("Response Code : " + response.getStatusLine().getStatusCode());
+
+        BufferedReader rd = new BufferedReader(
+                new InputStreamReader(response.getEntity().getContent()));
+
+        StringBuffer result = new StringBuffer();
+        String line = "";
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+
+        System.out.println(result.toString());**/
 
     }
 
