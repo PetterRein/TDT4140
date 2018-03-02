9faa22331b957ae4bebe517d313723527ceb3d5c
Mats Pedersen <matsjp@stud.ntnu.no>
2018-03-02 11:23:20 +0100

    Created a function that adds new users to the database. Created JUnit test for the new function. The JUnit test also tests that the login function works with the newly added users
    #11

---
+++ app.core/src/main/java/tdt4140/gr1844/app/core/Database.java
@@ -0,0 +1,26 @@
+package tdt4140.gr1844.app.core;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class Database {
+	public static void addUser(String role, String name, String email, String password) {
+		String salt = BCrypt.gensalt();
+		String passwordHash = BCrypt.hashpw(password, salt);
+		try {
+			SqlConnect conn = new SqlConnect();
+			PreparedStatement statement1 = conn.connect().prepareStatement("insert into users(role, name, email, passwordHash, salt) values(?, ?, ?, ?, ?)");
+			statement1.setString(1, role);
+			statement1.setString(2, name);
+			statement1.setString(3, email);
+			statement1.setString(4, passwordHash);
+			statement1.setString(5, salt);
+			statement1.executeUpdate();
+		}
+		catch(SQLException e) {
+			System.err.println(e);
+		}
+	}
+}
