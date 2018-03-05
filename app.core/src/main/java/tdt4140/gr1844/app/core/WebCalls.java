927d448689bc614aaa09ab343b9cb2b4500cb169
Petter Grø Rein <pettegre@stud.ntnu.no>
2018-03-05 12:25:39 +0100

    La til test #19

--- app.core/src/main/java/tdt4140/gr1844/app/core/WebCalls.java
+++ app.core/src/main/java/tdt4140/gr1844/app/core/WebCalls.java
@@ -1,11 +1,7 @@
 package tdt4140.gr1844.app.core;
 
-
-
-
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
-import org.apache.commons.httpclient.Cookie;
 import org.apache.http.client.CookieStore;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
@@ -15,15 +11,12 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-
 
 public class WebCalls {
     private final String USER_AGENT = "Mozilla/5.0";
@@ -37,13 +30,12 @@ public class WebCalls {
         System.out.println("\nTesting 2 - Send Http POST request");
         String user = "Petter";
         String password = "Test123";
-        http.sendPostUserLogin(user, password);
-        //http.sendPostOne("doctorID", "123");
-
+        http.sendPost(true, user, password);
+        http.sendPost(false, "doctorID", "123");
     }
 
     // HTTP GET request
-    private void sendGet() throws Exception {
+    public int sendGet() throws Exception {
         String url = "http://www.google.com/search?q=developer";
         HttpClient client = HttpClientBuilder.create().build();
         HttpGet request = new HttpGet(url);
@@ -59,10 +51,11 @@ public class WebCalls {
             result.append(line);
         }
         System.out.println(result.toString());
+        return response.getStatusLine().getStatusCode();
     }
 
     // HTTP POST request
-    private void sendPostUserLogin(String user, String password) throws Exception {
+    public int sendPost(boolean twoKeysOrNot, String user, String password) throws Exception {
         //Setter urlen vi sender til
         String url = "http://localhost:8080/webapi";
 
@@ -74,10 +67,17 @@ public class WebCalls {
         HttpPost httpPost = new HttpPost(url);
 
         //Legger ved parameterene til Posten
-        List<NameValuePair> params = new ArrayList<NameValuePair>();
-        params.add(new BasicNameValuePair("user", "John"));
-        params.add(new BasicNameValuePair("password", "pass"));
-        httpPost.setEntity(new UrlEncodedFormEntity(params));
+        if(twoKeysOrNot) {
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            params.add(new BasicNameValuePair("user", user));
+            params.add(new BasicNameValuePair("password", password));
+            httpPost.setEntity(new UrlEncodedFormEntity(params));
+        }
+        else {
+            List<NameValuePair> urlParameters = new ArrayList<>();
+            urlParameters.add(new BasicNameValuePair(user, password));
+            httpPost.setEntity(new UrlEncodedFormEntity(urlParameters));
+        }
 
         //Sender og lagrer svaret
         CloseableHttpResponse response = client.execute(httpPost);
@@ -97,40 +97,6 @@ public class WebCalls {
             result.append(line);
         }
         System.out.println(result.toString());
-
+        return response.getStatusLine().getStatusCode();
     }
-
-    private void sendPostOne(String key, String value) throws Exception {
-
-        String url = "http://localhost:8080/webapi";
-
-        HttpClient client = HttpClientBuilder.create().build();
-        HttpPost post = new HttpPost(url);
-
-        // add header
-        post.setHeader("User-Agent", USER_AGENT);
-
-        List<NameValuePair> urlParameters = new ArrayList<>();
-        urlParameters.add(new BasicNameValuePair(key, value));
-        post.setEntity(new UrlEncodedFormEntity(urlParameters));
-
-        HttpResponse response = client.execute(post);
-        System.out.println("\nSending 'POST' request to URL : " + url);
-        System.out.println("Post parameters : " + post.getEntity());
-        System.out.println("Response Code : " + response.getStatusLine().getStatusCode());
-
-        BufferedReader rd = new BufferedReader(
-                new InputStreamReader(response.getEntity().getContent()));
-
-        StringBuffer result = new StringBuffer();
-        String line = "";
-        while ((line = rd.readLine()) != null) {
-            result.append(line);
-        }
-
-        System.out.println(result.toString());
-
-    }
-
-
 }
---
+++ app.core/src/test/java/tdt4140/gr1844/app/core/WebCallsTest.java
@@ -0,0 +1,23 @@
+package tdt4140.gr1844.app.core;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class WebCallsTest {
+    private WebCalls http;
+
+    @Before
+    public void setUp() {
+        http = new WebCalls();
+    }
+
+    @Test
+    public void testGet() {
+        try {
+            Assert.assertEquals(200 , http.sendGet());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
