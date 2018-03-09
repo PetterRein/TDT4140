cf9e95da3ea32a069a2d751acf57b3749c7754cf
Balázs Orbán <info@balazsorban.com>
2018-03-09 11:22:25 +0100

    :green_heart: Fix CI build

--- app.core/src/main/java/tdt4140/gr1844/app/core/Database.java
+++ app.core/src/main/java/tdt4140/gr1844/app/core/Database.java
@@ -5,6 +5,8 @@ import java.sql.SQLException;
 
 import org.mindrot.jbcrypt.BCrypt;
 
+import javax.naming.NamingException;
+
 
 /**
  * Contains the methods for handling/mutating the database.
@@ -17,13 +19,14 @@ public class Database {
 	 * @param name The user's name.
 	 * @param email The user's email address.
 	 * @param password The user's password.
+	 * @param onlineOrOffline If the database is online or offline.
 	 */
-	public static void createUser(String role, String name, String email, String password) {
+	public static void createUser(String role, String name, String email, String password, boolean onlineOrOffline) throws NamingException {
 		String salt = BCrypt.gensalt();
 		String passwordHash = BCrypt.hashpw(password, salt);
 		try {
 			SqlConnect conn = new SqlConnect();
-			PreparedStatement statement1 = conn.connect().prepareStatement("insert into users(role, name, email, passwordHash, salt) values(?, ?, ?, ?, ?)");
+			PreparedStatement statement1 = conn.connect(onlineOrOffline).prepareStatement("insert into users(role, name, email, passwordHash, salt) values(?, ?, ?, ?, ?)");
 			statement1.setString(1, role);
 			statement1.setString(2, name);
 			statement1.setString(3, email);
@@ -39,11 +42,12 @@ public class Database {
 	/**
 	 * Deletes a user from the database by their e-mail address.
 	 * @param email The e-mail address of the user to be deleted.
+	 * @param onlineOrOffline If the database is online or offline.
 	 */
-	public static void deleteUser(String email) {
+	public static void deleteUser(String email, boolean onlineOrOffline) throws NamingException {
         try {
             SqlConnect conn = new SqlConnect();
-            PreparedStatement statement = conn.connect().prepareStatement("delete from users(email) where email='?'");
+            PreparedStatement statement = conn.connect(onlineOrOffline).prepareStatement("delete from users(email) where email='?'");
             statement.setString(1, email);
             statement.executeUpdate();
         }
