a186b402a28bd98822c2fdac142e924b32ddc26b
Petter Grø Rein <pettegre@stud.ntnu.no>
2018-03-16 11:52:06 +0100

    La til GUI example #16 #12

---
+++ app.ui/src/main/java/tdt4140/gr1844/app/ui/MainController.java
@@ -0,0 +1,53 @@
+package tdt4140.gr1844.app.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainController {
+    @FXML
+    private VBox jobsList;
+
+    @FXML
+    private AnchorPane rootPane;
+
+    @FXML
+    public void initialize() {
+        // Denne funksjonen blir kjørt automatisk når alt er loadet og du kan begynne å endre på ting.
+        addLoginElementsInList();
+    }
+
+    public void addLoginElementsInList() {
+        //Legge til default login knappene.
+
+        ArrayList<String> jobs = new ArrayList<>(Arrays.asList("Brukernavn", "Password", "Logginn"));
+        addButtons(jobs, jobsList);
+    }
+
+
+    public void addButtons(ArrayList<String> jobs, VBox jobsList) {
+        for (int i = 0; i < jobs.size(); i++) {
+            Button btnNumber = createButton(jobs.get(i));
+            jobsList.getChildren().add(btnNumber);
+        }
+    }
+
+    public Button createButton(String name) {
+        final Button button = new Button(name);
+        button.setId("jobButt");
+        button.setPrefSize(210, 50);
+        button.setOnMouseClicked(event -> {
+            try {
+                Main main = new Main();
+                main.changeView(rootPane, name);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        return button;
+    }
+}
