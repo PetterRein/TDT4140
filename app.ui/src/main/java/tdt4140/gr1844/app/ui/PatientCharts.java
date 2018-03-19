2ff3ddfad0856c0976cc4ebdf21e732624e94520
Johannes <torp95@gmail.com>
2018-03-19 12:39:57 +0100

    :sparkles: Adds patient chart files

---
+++ app.ui/src/main/java/tdt4140/gr1844/app/ui/PatientCharts.java
@@ -0,0 +1,12 @@
+package tdt4140.gr1844.app.ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class PatientCharts extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
---
+++ app.ui/src/main/java/tdt4140/gr1844/app/ui/PatientChartsController.java
@@ -0,0 +1,11 @@
+package tdt4140.gr1844.app.ui;
+
+import javafx.fxml.FXML;
+
+public class PatientChartsController {
+
+    @FXML
+    public void initialize() {
+    }
+
+}
