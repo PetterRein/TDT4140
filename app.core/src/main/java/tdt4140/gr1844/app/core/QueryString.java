307b2fae32354d10a4d21976649aedb4a8850d58
Balázs Orbán <balazor@stud.ntnu.no>
2018-04-10 01:30:54 +0200

    🔥simplify code
    The parse function now only returns a ```Map<String, String>``` instead of ```Map<String, List<String>>```

--- app.core/src/main/java/tdt4140/gr1844/app/core/QueryString.java
+++ app.core/src/main/java/tdt4140/gr1844/app/core/QueryString.java
@@ -19,17 +19,14 @@ public class QueryString {
                 .orElse("");
     }
 
-    public static Map<String, List<String>> parse(URL url) throws UnsupportedEncodingException {
-        final Map<String, List<String>> query_pairs = new LinkedHashMap<>();
+    public static Map<String, String> parse(URL url) throws UnsupportedEncodingException {
+        final Map<String, String> query_pairs = new LinkedHashMap<>();
         final String[] pairs = url.getQuery().split("&");
         for (String pair : pairs) {
             final int idx = pair.indexOf("=");
             final String key = idx > 0 ? URLDecoder.decode(pair.substring(0, idx), "UTF-8") : pair;
-            if (!query_pairs.containsKey(key)) {
-                query_pairs.put(key, new LinkedList<>());
-            }
             final String value = idx > 0 && pair.length() > idx + 1 ? URLDecoder.decode(pair.substring(idx + 1), "UTF-8") : null;
-            query_pairs.get(key).add(value);
+            query_pairs.put(key, value);
         }
         return query_pairs;
     }
