f9da8b6b658bb32e05bd7d3fb2ec377be156febc
Balázs Orbán <balazor@stud.ntnu.no>
2018-04-10 19:22:26 +0200

    ☔🎨 simplify test code

--- app.client/src/test/java/tdt4140/gr1844/app/client/WebCallsTest.java
+++ app.client/src/test/java/tdt4140/gr1844/app/client/WebCallsTest.java
@@ -2,34 +2,32 @@ package tdt4140.gr1844.app.client;
 
 import org.json.JSONObject;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import tdt4140.gr1844.app.core.QueryString;
+
 import java.net.URL;
 
 import static tdt4140.gr1844.app.core.createUrlFromString.createUrlFromStringMethode;
 
 public class WebCallsTest {
-    private WebCalls http;
 
-    @Before
-    public void setUp() { http = new WebCalls();
-    }
 
     @Test
     public void testGet() throws Exception {
         String stringURL = "action=createPatient&name=name&password=pw&email=email&doctorID=1";
         URL url = createUrlFromStringMethode(stringURL);
-        JSONObject json = http.sendGET(QueryString.parse(url));
-        if (json.getString("status").equals("OK")){
-            Assert.assertTrue(true);
-        }
-        else if (json.getString("status").equals("ERROR")){
-            String message = json.getString("message");
-            Assert.assertEquals("E-mail address (email) is taken.", message);
-        }
-        else{
-            Assert.assertTrue(false);
+        JSONObject json = WebCalls.sendGET(QueryString.parse(url));
+        switch (json.getString("status")) {
+            case "OK":
+                Assert.assertTrue(true);
+                break;
+            case "ERROR":
+                String message = json.getString("message");
+                Assert.assertEquals("E-mail address (email) is taken.", message);
+                break;
+            default:
+                Assert.fail();
+                break;
         }
     }
 }
--- app.server/src/test/java/tdt4140/gr1844/app/server/DatabaseTest.java
+++ app.server/src/test/java/tdt4140/gr1844/app/server/DatabaseTest.java
@@ -1,33 +1,26 @@
 package tdt4140.gr1844.app.server;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import javax.naming.NamingException;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class DatabaseTest {
-    boolean onlineOrOffline = false;
-	@Before
-	public void setUp() throws NamingException, IllegalAccessException, InstantiationException, ClassNotFoundException {
-		try {
-			SQL.initDatabase();
+import javax.naming.NamingException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-		}
-		catch(SQLException e){
-			System.err.println(e);
-			System.out.println("Setup failure");
-		}
+public class DatabaseTest {
+	@Before
+	public void setUp() throws IllegalAccessException, InstantiationException, ClassNotFoundException, SQLException {
+		SQL.initDatabase();
 	}
 	
 	@Test
-	public void addUserTest() throws NamingException, IllegalAccessException, ClassNotFoundException, InstantiationException, SQLException {
+	public void addUserTest() throws IllegalAccessException, ClassNotFoundException, InstantiationException, SQLException {
 		String email = "tom@doctor.com";
 		String password = "password";
 		Create.createPatient("tom", email, password, 2);
-		Assert.assertTrue(Authentication.login(email, password).get("status").toString().equals("OK"));
+		Assert.assertEquals("OK", Authentication.login(email, password).get("status").toString());
 	}
 
 	@Test
@@ -40,7 +33,7 @@ public class DatabaseTest {
 		String cookieAdmin = Authentication.login("tom", "tom").get("cookie").toString();
 		int idPatient = Authentication.login(email,password).getJSONObject("user").getInt("userId");
 		Delete.deleteUser(idPatient, cookieAdmin);
-		PreparedStatement statement = conn.connect().prepareStatement("select * from users where email = ?");
+		PreparedStatement statement = conn.connect().prepareStatement("SELECT * FROM users WHERE email = ?");
 		statement.setString(1, email);
 		statement.execute();
 		ResultSet rs = statement.getResultSet();
