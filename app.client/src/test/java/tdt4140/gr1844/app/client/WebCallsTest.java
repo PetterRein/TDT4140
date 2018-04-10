4dbd0b6e4010f7950a4a8a758e2adc2e519c8319
Petter Grø Rein <pettegre@stud.ntnu.no>
2018-04-10 19:11:42 +0200

    Fikset feil i WebCallsTest

--- app.client/src/test/java/tdt4140/gr1844/app/client/WebCallsTest.java
+++ app.client/src/test/java/tdt4140/gr1844/app/client/WebCallsTest.java
@@ -1,5 +1,6 @@
 package tdt4140.gr1844.app.client;
 
+import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,8 +18,18 @@ public class WebCallsTest {
 
     @Test
     public void testGet() throws Exception {
-        String stringURL = "action=createPatient&name=name&password=pw&email=email&doctorId=1";
+        String stringURL = "action=createPatient&name=name&password=pw&email=email&doctorID=1";
         URL url = createUrlFromStringMethode(stringURL);
-        Assert.assertEquals("OK", http.sendGET(QueryString.parse(url)).getString("status"));
+        JSONObject json = http.sendGET(QueryString.parse(url));
+        if (json.getString("status").equals("OK")){
+            Assert.assertTrue(true);
+        }
+        else if (json.getString("status").equals("ERROR")){
+            String message = json.getString("message");
+            Assert.assertEquals("E-mail address (email) is taken.", message);
+        }
+        else{
+            Assert.assertTrue(false);
+        }
     }
 }
